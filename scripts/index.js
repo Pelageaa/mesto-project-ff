b996ea1a6b3c9b6a8ba9a15b29844ea37729855f
Polina Maksimenko <polina-maksimenko@inbox.ru>
2024-04-29 23:00:40 +0700

    correct

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,27 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
+
 // @todo: DOM узлы
 const cardList = document.querySelector(".places__list");
+
 // @todo: Функция создания карточки
 function createCard(cardData, removeCard) {
+  const { link, name } = cardData;
   const card = cardTemplate.querySelector(".places__item").cloneNode(true);
   const cardImage = card.querySelector(".card__image");
-  cardImage.src = cardData.link; // Устанавливаем src для изображения
-  cardImage.alt = cardData.name; // Устанавливаем alt для изображения
-  card.querySelector(".card__title").textContent = cardData.name;
-  const deleteCard = card.querySelector(".card__delete-button");
-  deleteCard.addEventListener("click", () => removeCard(card));
-  return card;
+  cardImage.src = link; // Устанавливаем ссылку на изображение
+  cardImage.alt = name; // Устанавливаем текст для атрибута alt
+  card.querySelector(".card__title").textContent = name;
+  const deleteButton = card.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => removeCard(card));
+  return card; 
 }
+
 // @todo: Функция удаления карточки
 function deleteButton(cardElement) {
   cardElement.remove();
 }
+
 // @todo: Вывести карточки на страницу
 initialCards.forEach((cardData) => {
   const cardElement = createCard(cardData, deleteButton);
